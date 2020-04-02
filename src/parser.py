0a8583206ea3712f785454a1068e19c0e5b18c83
BrotherMaestro <hsipper@optusnet.com.au>
2020-04-02 17:24:41 +1100

    PARSER STRUCTURES COMPLETE

--- src/parser.py
+++ src/parser.py
@@ -6,6 +6,10 @@ import csv
 
 class Parse:
 
+    TRAINCSV = "data/training.csv"
+    TESTCSV = "data/test.csv"
+    CSVKEYS = ['No', 'Words', 'Topic']
+
     ### FIRST STRUCTURE ###
     # Dictionary of each column
     # List of article numbers
@@ -26,28 +30,137 @@ class Parse:
     
     def __init__(self, style=1):
         if style == 1:
-            self.trainSet = dict()
-            style1("data/training.csv")
-            self.testSet = dict()
+            self.trainSet = self.style1(self.TRAINCSV)
+            self.testSet = self.style1(self.TESTCSV)
         elif style == 2:
-            self.trainSet = []
-            self.testSet = []
+            self.trainSet = self.style2(self.TRAINCSV)
+            self.testSet = self.style2(self.TESTCSV)
         elif style == 3:
-            self.trainSet = dict()
-            self.testSet = dict()
+            self.trainSet = self.style3(self.TRAINCSV)
+            self.testSet = self.style3(self.TESTCSV)
         elif style == 4:
-            self.trainSet = dict()
-            self.testSet = dict()
+            self.trainSet = self.style4(self.TRAINCSV)
+            self.testSet = self.style4(self.TESTCSV)
         else:
             print("ERROR: Please give a style number")
+    
+    
+    ################
+    ### METHODS ####
+    ################
+    
+    
+    ### GETTERS ###
+    def getTrain(self):
+        return self.trainSet
         
+    def getTest(self):
+        return self.testSet
+        
+    
+    
+    # words: A list of words
+    # Output: A dictionary, performing a frequency count of each word
+    def buildWordDict(self, words):
+        freqCount = dict()
+        for entry in words:
+            if entry in freqCount:
+                freqCount[entry] += 1
+            else:
+                freqCount[entry] = 1
+        return freqCount
+    
+    
+    #### STRUCTURAL PARSERS BY INIT DEFINITION 1 -> 4 ###
+    
+    #Dict of columns, Words as Dict
     def style1(self, fileName):
         with open(fileName, newline='') as f:
-            next(f)
-            preformat = csv.DictReader(f, ['No', 'Words', 'Topic'])
+            next(f) #skip column names
+            preformat = csv.DictReader(f, self.CSVKEYS)
             
-            print(preformat['No'])
-            print(preformat['Topic'])
+            #Build Column Dictionary => Will always be first time
+            Set = {'No':[], 'Words':[], 'Topic':[]}
+            for row in preformat:
+                words = row['Words'].split(',')
+                wordDict = self.buildWordDict(words)
+                
+                Set['No'].append(int(row['No']))
+                Set['Words'].append(wordDict)
+                Set['Topic'].append(row['Topic'])
+            
+            return Set
+           
+    #List of Lists
+    def style2(self, fileName):
+        with open(fileName, newline='') as f:
+            next(f)
+            preformat = csv.DictReader(f, self.CSVKEYS)
+            
+            #create column lists
+            No = []
+            Words = []
+            Topic = []
+            
+            #append to columns by row
+            for row in preformat:
+                No.append(int(row['No']))
+                Words.append(row['Words'].split(','))
+                Topic.append(row['Topic'])
+            
+            #Push into a List of Lists
+            #Access elems by selecting column then row, eg: arr2D[1][500], fifth hundred entry, containing list of words
+            return [No, Words, Topic]
+            
+            
+    #Article No Dict (always from zero), Words as Dict
+    def style3(self, fileName):
+        with open(fileName, newline='') as f:
+            next(f)
+            preformat = csv.DictReader(f, self.CSVKEYS)
+            
+            #Create a dictionary to store entries by row
+            Set = {}
+            init = 0
+            for row in preformat:
+                if init < 1:
+                    init += 1
+                    offset = int(row['No'])
+                ArticleNo = int(row['No']) - offset
+                if ArticleNo in Set:
+                    continue
+                else:
+                    words = row['Words'].split(',')
+                    wordDict = self.buildWordDict(words)
+                    Set[ArticleNo] = [wordDict, row['Topic']]
+                    
+            return Set
+            
+    #Article No Dict (always from zero), Words as List
+    def style4(self, fileName):
+        with open(fileName, newline='') as f:
+            next(f)
+            preformat = csv.DictReader(f, self.CSVKEYS)
         
+            #Create a dictionary to store entries by row
+            Set = {}
+            init = 0
+            for row in preformat:
+                if init < 1:
+                    init += 1
+                    offset = int(row['No'])
+                ArticleNo = int(row['No']) - offset
+                if ArticleNo in Set:
+                    continue
+                else:
+                    words = row['Words'].split(',')
+                    Set[ArticleNo] = [words, row['Topic']]
+                
+            return Set
+
+
+##############################
+### END STRUCTURAL PARSERS ###
+##############################
 
 
--- src/test_parser.py
+++ src/test_parser.py
@@ -4,9 +4,46 @@ __version__ = '0.1.0'
 from parser import Parse
 
 def main():
+    #testParseForm1()
+    #testParseForm2()
+    #testParseForm3()
+    testParseForm4()
+
+def testParseForm1():
     parseClass = Parse(1)
-    print(parseClass)
+    TrainSet = parseClass.getTrain()
+    TestSet = parseClass.getTest()
     
+    #print(TrainSet['No'])
+    #print(TrainSet['Words'])
+    #print(TrainSet['Topic'])
+
+def testParseForm2():
+    parseClass = Parse(2)
+    TrainSet = parseClass.getTrain()
+    TestSet = parseClass.getTest()
+    
+    #print(TrainSet[1][500])
+    #print(TestSet[1][100])
+
+
+def testParseForm3():
+    parseClass = Parse(3)
+    TrainSet = parseClass.getTrain()
+    TestSet = parseClass.getTest()
+    
+    print(TrainSet[0])
+    print(TestSet[0])
+
+def testParseForm4():
+    parseClass = Parse(4)
+    TrainSet = parseClass.getTrain()
+    TestSet = parseClass.getTest()
+    
+    print(TrainSet[0])
+    print(TestSet[0])
+    
+
 main()
 
         
